4f9e5cfbd2e7eb6fdc83a3c008d547a68de96288
Markus Gotzmann <markus@itgrity.de>
2023-10-25 17:14:22 +0200

    Setup Repo

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h-screen bg-gray-950">
+        <div class="container"></div>
+            <div class="p-8 shadow-lg">
+                <ul>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        <h1 class="text-[#03fca5] text-9xl"> Willkommen</h1>
+        <contact/>
+    </div>
+</template>
+
+<script>
+</script>
